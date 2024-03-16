6359683986b65dc92f0058b2e437f66aa306e86b
John Garner <66663652+JohnGarnerIII@users.noreply.github.com>
2024-03-16 05:33:05 -0400

    I don't know what I'm doing

--- background.js
+++ background.js
@@ -1,4 +1,38 @@
-let audio = new Audio('https://relay0.r-a-d.io/main.mp3');
+let audio;
+
+function getAudio() {
+    if (!audio) {
+        audio = new Audio('https://relay0.r-a-d.io/main.mp3');
+    }
+    return audio;
+}
+
+browser.runtime.onMessage.addListener((message) => {
+    const audio = getAudio();
+    if (message.command === 'togglePlay') {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    } else if (message.command === 'changeVolume') {
+        audio.volume = message.volume;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+// Yeah...
+
+/* let audio = new Audio('https://relay0.r-a-d.io/main.mp3');
 
 browser.runtime.onMessage.addListener((message) => {
     if (message.command === 'togglePlay') {
@@ -10,4 +44,4 @@ browser.runtime.onMessage.addListener((message) => {
     } else if (message.command === 'changeVolume') {
         audio.volume = message.volume;
     }
-});
+}); */
--- popup.js
+++ popup.js
@@ -9,10 +9,22 @@ document.getElementById('toggleButton').addEventListener('click', function() {
   }
 });
 
+
+// When the popup is loaded, set the volume slider to the last saved value
+document.addEventListener('DOMContentLoaded', (event) => {
+  const savedVolume = localStorage.getItem('volume');
+  if (savedVolume) {
+      document.getElementById('volumeSlider').value = savedVolume;
+      browser.runtime.sendMessage({command: 'changeVolume', volume: savedVolume});
+  }
+});
+
 document.getElementById('toggleButton').addEventListener('click', function() {
   browser.runtime.sendMessage({command: 'togglePlay'});
 });
 
 document.getElementById('volumeSlider').addEventListener('input', function() {
+  // Save the volume level when it's changed
+  localStorage.setItem('volume', this.value);
   browser.runtime.sendMessage({command: 'changeVolume', volume: this.value});
 });
